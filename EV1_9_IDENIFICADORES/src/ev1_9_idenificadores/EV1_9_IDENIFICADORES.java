3ffd1b98e4732a07107c6a13d9664cd0e9d97d75
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-09-13 17:56:23 -0600

    Add files via upload

---
+++ EV1_9_IDENIFICADORES/src/ev1_9_idenificadores/EV1_9_IDENIFICADORES.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ev1_9_idenificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EV1_9_IDENIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

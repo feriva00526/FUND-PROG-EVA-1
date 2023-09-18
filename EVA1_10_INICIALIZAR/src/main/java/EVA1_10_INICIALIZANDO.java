ecb897e38c51331f830307aecccb968d957129f6
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-09-18 17:55:05 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZAR/src/main/java/EVA1_10_INICIALIZANDO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZANDO {
+   public static void main (String[]args){
+      
+      int valor = 0;
+      System.out.println(valor);
+      
+      int precio;
+      precio = 100;
+      
+      double salario = 0, precioMayoreo = 100, iva = 10.5 , subtotal = 15, total = 100.0;
+      
+      final int conteo;
+      conteo = 100;
+      System.out.println(conteo);
+     
+      final double MI_VALOR_PI = 3.1416;
+      final String ESCUELA ="Instituto Tecnologico de CHIHUAHUA II";
+      final int CALIFA_PASAR = 70;
+      /*
+         Varias lineas de comentarios cuando tirar
+      mucho rollo esta en la mejor opcion
+      */
+      
+       /*final double MI_VALOR_PI = 3.1416;
+      final String ESCUELA ="Instituto Tecnologico de CHIHUAHUA II";
+      final int CALIFA_PASAR = 70;*/
+       
+      int x; //Comentario de una sola linea, todo en linea es un comentario
+}
+}
